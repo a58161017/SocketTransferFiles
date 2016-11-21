8c636e45b36c6b469b43cb33a737cb7b7a401c80
kiuno <a58161017@gmail.com>
2016-11-21 20:53:14 +0800

    add file: config.ini set parameter

--- Server/src/kiuno/network/socket/ReceiverFiles.java
+++ Server/src/kiuno/network/socket/ReceiverFiles.java
@@ -1,8 +1,6 @@
 package kiuno.network.socket;
 
-import java.io.DataInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,6 +11,8 @@ public class ReceiverFiles extends Thread {
 	private String ip = "127.0.0.1"; //預設IP
 	private int port = 51239; //預設Port
 	private int backlog = 100; //在佇列端請求連線的最大長度
+	private String output = ""; //檔案輸出路徑
+	private int filesize = 30*1024*1024; //可接受的檔案大小，預設30M
 	private ServerSocket serverSocket = null;
 	/*backlog 範例說明:
 		(1)int backlog = 2
@@ -22,14 +22,11 @@ public class ReceiverFiles extends Thread {
 		(5)一個client D連接進來，server正在處理A，但是，佇列已經達到最大值2，因此拒絕D的連線請求
 		(6)直到A處理完後，server accept B，也就是將B從佇列取出(FIFO:先進先出)，此時下一個client才能連進來
 	*/
-	ReceiverFiles(String ip, int port, int backlog){
-		this.ip = ip;
-		this.port = port;
-		this.backlog = backlog;
-	}
 	
 	public void start(){
 		try{
+			initial();
+			checkOutput();
 			serverSocket = new ServerSocket(port, backlog, InetAddress.getByName(ip));
 		}catch(Exception e){
 			System.out.println("ServerSocket建立有問題");
@@ -52,6 +49,43 @@ public class ReceiverFiles extends Thread {
 		}
 	}
 	
+	private void initial(){
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("config.ini"));
+			String line;
+			while ((line=br.readLine()) != null) {
+				if(line.contains("=")) parseIni(line);
+			}
+		}catch (IOException e) {System.out.println(e);
+		}finally{ 
+			if(br != null){
+				try{br.close();}catch(Exception e){};
+			}
+		}
+	}
+	
+	private void parseIni(String line){
+		String[] str = line.split("=");
+		if("ip".equals(str[0].toLowerCase()))
+			this.ip = str[1];
+		else if("port".equals(str[0].toLowerCase()))
+			this.port = Integer.parseInt(str[1]);
+		else if("backlog".equals(str[0].toLowerCase()))
+			this.backlog = Integer.parseInt(str[1]);
+		else if("output".equals(str[0].toLowerCase()))
+			this.output = str[1];
+		else if("filesize".equals(str[0].toLowerCase()))
+			this.filesize = Integer.parseInt(str[1]);
+	}
+	
+	private void checkOutput(){
+		if(!"".equals(output.trim())){
+			File f = new File(this.output);
+			if(!f.exists()) f.mkdirs();
+		}
+	}
+	
 	private void saveFile(Socket clientSock) {
 		Utility ut = new Utility();
 		DataInputStream dis = null;
@@ -59,13 +93,12 @@ public class ReceiverFiles extends Thread {
 		try{
 			System.out.println("接收到檔案，準備處理");
 			dis = new DataInputStream(clientSock.getInputStream());
-			fos = new FileOutputStream("D:/FTP/kiuno/AppFiles/P"+ut.getDateTime()+".jpg");
+			fos = new FileOutputStream(output + "/P"+ut.getDateTime()+".jpg");
 			byte[] buffer = new byte[4096];
 			
-			int filesize = 30*1024*1024; //可接受的檔案大小，預設30M
 			int read = 0;
 			int totalRead = 0;
-			int remaining = filesize;
+			int remaining = this.filesize;
 			while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
 				totalRead += read;
 				remaining -= read;
--- Server/src/kiuno/network/socket/ReceiverFilesExecute.java
+++ Server/src/kiuno/network/socket/ReceiverFilesExecute.java
@@ -3,7 +3,7 @@ package kiuno.network.socket;
 public class ReceiverFilesExecute {
 
 	public static void main(String[] args) {
-		ReceiverFiles rf = new ReceiverFiles("127.0.0.1",51239,100);
+		ReceiverFiles rf = new ReceiverFiles();
 		rf.start();
 	}
 }
